98d56aab257fb3d92e96e290ad8ec1203f7ff784
umanari145 <umanari145@gmail.com>
2023-12-30 00:07:04 +0900

    markdownエディタ

---
+++ front/src/libs/MarkdonwStyle.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+
+export const Header = styled.header`
+  font-size: 1.5rem;
+  height: 10rem;
+  left: 0;
+  line-height: 1rem;
+  padding: 0.5rem 1rem;
+  position: fixed;
+  right: 0;
+  top: 0;
+`
+
+export const H1 = styled.h1`
+  color:#FF0000;
+`
+
+export const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 5rem;
+  display:flex;
+`
+
+export const TextArea = styled.textarea`
+  border-right: 1px solid silver;
+  border-top: 1px solid silver;
+  width:50%;
+  font-size: 1rem;
+  padding: 0.5rem;
+`
+
+export const Preview = styled.div`
+  border-top: 1px solid silver;
+  overflow-y: scroll;
+  padding: 1rem;
+  width: 50%;
+`
--- front/src/pages/Markdown.tsx
+++ front/src/pages/Markdown.tsx
@@ -1,12 +1,26 @@
-export const Markdown = () => {
+import { ChangeEvent, FC, useState } from "react";
+import {Header, Wrapper, TextArea, Preview, H1} from "../libs/MarkdonwStyle";
+
+export const Markdown:FC = () => {
+  const StorageKey = 'pages:markdown';
+  const [inputValue, setInputValue] = useState<string>(localStorage.getItem(StorageKey) || '');
+
+  const handleChange = (e:ChangeEvent<HTMLTextAreaElement>) => {
+    const textValue = e.currentTarget.value;
+    localStorage.setItem(StorageKey, textValue)
+    setInputValue(textValue)
+  }
 
   return (
     <>
-     <div>
-       マークダウン
-     </div>
-     <ul>
-      </ul>     
+     <Header>
+       <H1>マークダウン</H1>
+     </Header>
+     <Wrapper>
+       <TextArea value={inputValue} onChange={(e) => handleChange(e)}>
+       </TextArea>
+       <Preview><pre>{inputValue}</pre></Preview>  
+     </Wrapper>
     </>
   )
 }
